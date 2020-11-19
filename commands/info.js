765722e108259d3c99b6d30655d7425869ed84d8
PlayHive <64799331+PlayHive@users.noreply.github.com>
2020-11-19 21:36:25 +0100

    Update info.js

--- commands/info.js
+++ commands/info.js
@@ -8,7 +8,7 @@ module.exports = {
         .setTitle('**INFO**') 
         .setDescription('RoBot is a multi purpose bot with many fun such as helpful commands! The Prefix of RoBot is R! so when you type a command remember to have it starting with R!') 
         .addField(':tada: Invite RoBot today! :tada:', 'https://discord.com/oauth2/authorize?client_id=737647174959235162&permissions=8&scope=bot') 
-        .setThumbnail('https://discordapp.com/channels/712709770779557894/712709770817175603/776093390630682656') 
+        .setThumbnail('https://cdn.discordapp.com/attachments/712709770817175603/779082344501084200/450-200709182334532329.jpg') 
         .setFooter('Made By Alicoolguy') 
         .setColor('BLUE') 
         message.channel.send(infoembed) 
